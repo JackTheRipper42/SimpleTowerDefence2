c16ceb13265f406bd1918955e61abd062164b6ce
Jack the Ripper <andreastheripper@aol.de>
2016-06-14 21:24:06 +0200

    resolution and window mode can be set in the options menu

--- Assets/Scripts/OptionsPanel.cs
+++ Assets/Scripts/OptionsPanel.cs
@@ -7,21 +7,30 @@ namespace Assets.Scripts
     public class OptionsPanel : MonoBehaviour
     {
         public Dropdown QualityDropdown;
+        public Dropdown ResolutionDropdown;
+        public Toggle WindowedToggle;
 
         public void OkClicked()
         {
             QualitySettings.SetQualityLevel(QualityDropdown.value);
+            var resolution = Screen.resolutions[ResolutionDropdown.value];
+            Screen.SetResolution(resolution.width, resolution.height, !WindowedToggle.isOn, resolution.refreshRate);
         }
 
         public void CancelClicked()
         {
             QualityDropdown.value = QualitySettings.GetQualityLevel();
+            ResolutionDropdown.value = Screen.resolutions.ToList().IndexOf(Screen.currentResolution);
+            WindowedToggle.isOn = !Screen.fullScreen;
         }
 
         protected virtual void Start()
         {
             QualityDropdown.AddOptions(QualitySettings.names.ToList());
             QualityDropdown.value = QualitySettings.GetQualityLevel();
+            ResolutionDropdown.AddOptions(Screen.resolutions.Select(resolution => resolution.ToString()).ToList());
+            ResolutionDropdown.value = Screen.resolutions.ToList().IndexOf(Screen.currentResolution);
+            WindowedToggle.isOn = !Screen.fullScreen;
         }
     }
 }
